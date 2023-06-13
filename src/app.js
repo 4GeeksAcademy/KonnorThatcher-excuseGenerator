941dc6a9212bcca4b0399e538c677e8cfc5617e7
KonnorThatcher <131403452+KonnorThatcher@users.noreply.github.com>
2023-06-13 01:06:42 +0000

    Javascript part done, need to add modal to html

--- src/app.js
+++ src/app.js
@@ -1,11 +1,48 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse-generator").innerHTML = `"${excuseGenerator(sentenceStructure)}"`;
 };
+
+let whoDunnit = [
+  "My brother-in-law's second cousin's friend from Wichita",
+  "Frankie, that guy who stole my lunch money in 3rd grade that one time,",
+  "My dentist's dentist",
+  "My computer, who believe it or not is actually sentient,"
+];
+
+let actionThatHappened = [
+  "eviscerated",
+  "demolished",
+  "went back in time and erased from existence",
+  "lit on fire",
+  "ate for brunch"
+];
+
+let thingIWasSupposedToAttendTo = [
+  "the code I needed to complete this project",
+  "the rent money, and I got kicked out of my house",
+  "the steering wheel of my car",
+  "my internet router"
+];
+
+let whenOrWhereItHappened = [
+  "while I was on a trip.",
+  "before I could give you that thing you wanted.",
+  "while I was interrupted by my neighbor, who was trying to show me his new favorite magic trick.",
+  "when I went to the store to buy a tub of vanilla bean ice cream."
+];
+
+let sentenceStructure = [whoDunnit, actionThatHappened, thingIWasSupposedToAttendTo, whenOrWhereItHappened];
+
+let excuseGenerator = (array) => {
+  const sentence = [];
+  for (let i of array) {
+    let rng = Math.floor(Math.random() * array.length);
+    console.log(rng);
+    sentence.push(i[rng]);
+  }
+  return sentence.join(' ');
+}
